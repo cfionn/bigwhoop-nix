1002f9a9fa27a915a1d95f57a30c9adf03aa355c
cfionn <c.findlay14@outlook.com>
2026-06-09 23:03:11 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,134 +1,106 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
+{ config, pkgs, ... }:
 {
-  config,
-  pkgs,
-  ...
-}: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ./home-manager.nix
-    <home-manager/nixos>
-  ];
-
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nebulaNugget"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  # --- tpm2 stuffy
+  boot.initrd.systemd.enable = true;
+  boot.initrd.luks.devices."luks-8d4fec51-dfcc-4c14-87bb-baf473ba01f4" = {
+    device = "/dev/disk/by-uuid/8d4fec51-dfcc-4c14-87bb-baf473ba01f4";
+    crypttabExtraOpts = [ "tpm2-device=auto" ];
+  };
+  # enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "solidus";
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Hardware Stuff
-  # This is specific to getting my webcam to work on my 2015 MacBook Air
-  boot.kernelModules =["w1"];
-  boot.extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
-  hardware.facetimehd.enable = true;
-  hardware.facetimehd.withCalibration = true;
-
   # Set your time zone.
-  time.timeZone = "America/Boise";
-
+  time.timeZone = "Europe/London";
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
+  i18n.defaultLocale = "en_GB.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us";
-    variant = "mac";
+    layout = "gb";
+    variant = "";
   };
-
+  # Configure console keymap
+  console.keyMap = "uk";
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    # media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
+  # Define a user account. Don't forget to set a password with 'passwd'.
+  users.users."fionn" = {
+    isNormalUser = true;
+    description = "fionn";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  # Mount drives
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+  security.polkit.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.udisks2") === 0 && subject.local && subject.active) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+  systemd.user.services.automount-drives = {
+    description = "Auto mount drives via udisksctl";
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeShellScript "mount-drives" ''
+        ${pkgs.udisks2}/bin/udisksctl unmount --block-device /dev/disk/by-uuid/7aeb75d7-8b66-433c-a642-693c43d2726a --no-user-interaction || true
+        ${pkgs.udisks2}/bin/udisksctl unmount --block-device /dev/disk/by-uuid/c99b8592-cb3c-43e4-9859-7fe6f323655b --no-user-interaction || true
+        sleep 1
+        ${pkgs.udisks2}/bin/udisksctl mount --block-device /dev/disk/by-uuid/7aeb75d7-8b66-433c-a642-693c43d2726a --no-user-interaction || true
+        ${pkgs.udisks2}/bin/udisksctl mount --block-device /dev/disk/by-uuid/c99b8592-cb3c-43e4-9859-7fe6f323655b --no-user-interaction || true
+      '';
+    };
+  };
   # Install firefox.
-  # programs.firefox.enable = true;
-
+  programs.firefox.enable = true;
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
     git
-    chromium
-    guvcview
-    # zoom-us
-    # nodejs
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "26.05";
 }
