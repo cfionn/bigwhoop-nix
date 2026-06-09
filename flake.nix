c26836d9d04106c0aff6dc509989b358d496aa1c
cfionn <c.findlay14@outlook.com>
2026-06-09 23:03:48 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+     nixosConfigurations = {
+	solidus = nixpkgs.lib.nixosSystem {
+	   system = "x86_64-linux"; 
+	   modules = [
+		./configuration.nix
+	   ];
+	};
+     };
+  };
+}
